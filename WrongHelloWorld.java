3fcdcf2c52b9ab725bd599eafec7a2ae8c5bcaf4
ERNST Michael <michaelsb.ernst@gmail.com>
2025-07-28 18:13:37 +0200

    Added WrongHelloWorld for SonarQube

---
+++ WrongHelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Affiche un message de bienvenue
+        System.out.println("Démonstration d'une erreur pour SonarQube !");
+        
+        // Crée deux objets String qui ont le même contenu
+        String s1 = new String("test");
+        String s2 = new String("test");
+
+        // ERREUR VOLONTAIRE : On compare les objets et non leur contenu.
+        // SonarQube devrait détecter que cette condition est toujours fausse.
+        if (s1 == s2) {
+            System.out.println("Les chaînes sont identiques.");
+        } else {
+            System.out.println("Les chaînes ne sont PAS identiques.");
+        }
+    }
+}
